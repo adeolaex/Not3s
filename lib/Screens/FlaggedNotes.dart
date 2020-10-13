1a20285a3cc1db141ecfb7ad31a1893732912d93
Adeola <inboxadeola@gmail.com>
2020-10-13 16:38:39 +0200

    file issues

---
+++ lib/Screens/FlaggedNotes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+// The screen for notes that have been Flagged Notes
+
+class FlaggedNotes extends StatefulWidget {
+  @override
+  _FlaggedNotesState createState() => _FlaggedNotesState();
+}
+
+class _FlaggedNotesState extends State<FlaggedNotes> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
